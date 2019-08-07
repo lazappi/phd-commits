d0644da34101f3d8dd0110a213a61dbd5123e3eb
Luke Zappia <lazappi@users.noreply.github.com>
2019-08-07 15:10:44 +1000

    Add repository labels
    Kind of works...

--- plot_commits.R
+++ plot_commits.R
@@ -1,4 +1,6 @@
 plot_commits <- function(commits, repositories) {
+    `%>%` <- magrittr::`%>%`
+
     commits <- dplyr::left_join(commits, repositories, by = "Repository")
 
     if (any(is.na(commits$Category))) {
@@ -19,6 +21,25 @@ plot_commits <- function(commits, repositories) {
                            "Analysis", "Reports", "Other", "Side projects")))
         )
 
+    first_commits <- commits %>%
+        dplyr::group_by(Repository) %>%
+        dplyr::arrange(When) %>%
+        dplyr::filter(dplyr::row_number() == 1) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(Repository = stringr::str_trunc(Repository, 13))
+
+    first_commits_top <- first_commits %>%
+        dplyr::group_by(Category) %>%
+        dplyr::filter(dplyr::row_number() %% 2 == 1) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(vjust = rep(c(3, 5), length.out = n()))
+
+    first_commits_bottom <- first_commits %>%
+        dplyr::group_by(Category) %>%
+        dplyr::filter(dplyr::row_number() %% 2 == 0) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(vjust = rep(c(3, 5), length.out = n()))
+
     pal <- c(
         "#00ADEF", # Blue
         "#8DC63F", # Green
@@ -33,7 +54,21 @@ plot_commits <- function(commits, repositories) {
         commits,
         ggplot2::aes(x = Category, y = When, colour = Category, shape = Type)
     ) +
-        ggplot2::geom_jitter(width = 0.2, height = 0, alpha = 0.8, size = 2) +
+        ggplot2::geom_jitter(width = 0.1, height = 0, alpha = 0.8, size = 2) +
+        #ggplot2::geom_text(data = first_commits, aes(label = Repository),
+        #                   vjust = -2) +
+        ggrepel::geom_text_repel(
+            data = first_commits_top,
+            aes(label = Repository, vjust = -vjust),
+            size = 3,
+            direction = "x"
+        ) +
+        ggrepel::geom_text_repel(
+            data = first_commits_bottom,
+            aes(label = Repository, vjust = vjust),
+            size = 3,
+            direction = "x"
+        ) +
         ggplot2::scale_x_discrete(drop = FALSE) +
         ggplot2::scale_shape_manual(values = c(16, 1)) +
         ggplot2::scale_colour_manual(values = rev(pal)) +
